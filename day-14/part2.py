591e415e4484f8f682b30e7f0c636bc28afbf665
Ivo Verberk <iverberk@xebia.com>
2018-12-29 16:58:21 +0100

    Day 14 solutions.

---
+++ day-14/part2.py
@@ -0,0 +1,15 @@
+scores = '37'
+first_recipe = 0
+second_recipe = 1
+recipes = '540561'
+
+while recipes not in scores[-7:]:
+    first_score = int(scores[first_recipe])
+    second_score = int(scores[second_recipe])
+
+    scores += str(first_score + second_score)
+
+    first_recipe = (first_recipe + first_score + 1) % len(scores)
+    second_recipe = (second_recipe + second_score + 1) % len(scores)
+
+print(scores.index(recipes))
